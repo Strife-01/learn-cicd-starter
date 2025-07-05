5f80e34733dde3405d55e2fd895bbef17c630768
Strife01 <andreiursachi10@yahoo.com>
2025-07-05 20:05:11 +0100

    fix: go issues as per gosec

--- json.go
+++ json.go
@@ -30,5 +30,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	if w.Write(dat) != nil {
+		w.WriteHeader(500)
+	}
 }
